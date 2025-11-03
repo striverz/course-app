7fde34b96b91e847906a6e47c1447e395061b7d2
striverz <striverzarc@gmail.com>
2025-11-03 13:41:41 +0530

    added the courser registeration through user not admin

--- router/courseController.js
+++ router/courseController.js
@@ -1,12 +1,39 @@
 const express = require("express");
 const { authUser } = require("../middlewares/authUser");
+const { authAdmin } = require("../middlewares/authAdmin");
+const { CourseModel } = require("../models/courseModel");
 const courseController = express.Router();
 
-courseController.get("/preview", authUser, (req, res) => {
-  res.json({
-    message: "No Course as of now...",
-    userId: req.userId,
-  });
+courseController.post("/register", authUser, async (req, res) => {
+  try {
+    const { imageUrl, title, price, description } = req.body;
+
+    await CourseModel.create({
+      imageUrl,
+      title,
+      price,
+      description,
+    });
+    res.json({
+      message: "Course Created Successfully",
+    });
+  } catch (err) {
+    res.status(401).json({
+      message: err.message,
+    });
+  }
+});
+courseController.get("/preview", authUser, async (req, res) => {
+  try {
+    const courses = await CourseModel.find({});
+    res.json({
+      courses,
+    });
+  } catch (err) {
+    res.status(401).json({
+      message: err.message,
+    });
+  }
 });
 
 module.exports = {
